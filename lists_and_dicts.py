c3be24eb69ba3c8da88a613e60954c197c127866
Andres <bdswtechstuff@gmail.com>
2021-08-04 15:09:56 -0500

    Agregué el super_dict y un for loop iterando entre llaves y valores de super_dict

--- lists_and_dicts.py
+++ lists_and_dicts.py
@@ -13,6 +13,14 @@ def run():
 
     return super_list
     
+    super_dict = {
+        'natural_nums': [1, 2, 3, 4, 5],
+        'integer_nums': [-1, -2, 0, 1, 2],
+        'floating_nums': [1, 1.1, 1.2, 1.5, 5.6]
+    }
+
+    for k, v in super_dict.items():
+        print(k, '-', v)
 
 if __name__ == '__main__':
     runned = run()
